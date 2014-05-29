8e132de0efd57da675f9fa44ee0783e3d2d0202e
Brian Sipple <Bsipple57@gmail.com>
2014-05-29 15:52:44 -0500

    Week 3 Assignment complete

--- assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleAtomicLong.java
+++ assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleAtomicLong.java
@@ -1,9 +1,7 @@
-// Import the necessary Java synchronization and scheduling classes.
-
 package edu.vuum.mocca;
 
+import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.concurrent.locks.Lock;
 
 /**
  * @class SimpleAtomicLong
@@ -14,67 +12,131 @@ import java.util.concurrent.locks.Lock;
  */
 class SimpleAtomicLong
 {
-    /**
-     * The value that's manipulated atomically via the methods.
-     */
-    private long mValue;
+	/**
+	 * The value that's manipulated atomically via the methods.
+	 */
+	private long mValue;
+
+	/**
+	 * The ReentrantReadWriteLock used to serialize access to mValue.
+	 */
+
+	// TODO -- you fill in here by replacing the null with an
+	// initialization of ReentrantReadWriteLock.
+	private ReadWriteLock mRWLock = new ReentrantReadWriteLock(false);
+
+	/**
+	 * Creates a new SimpleAtomicLong with the given initial value.
+	 */
+	public SimpleAtomicLong(long initialValue)
+	{
+		// TODO -- you fill in here
+		this.mValue = initialValue;
+	}
+
+	/**
+	 * @brief Gets the current value.
+	 * 
+	 * @returns The current value
+	 */
+	public long get()
+	{
+		long value;
+
+		// TODO -- you fill in here
+		mRWLock.readLock().lock();
+		value = mValue;	
+		mRWLock.readLock().unlock();
+		return value;
+
+	}
+
+	/**
+	 * @brief Atomically decrements by one the current value
+	 *
+	 * @returns the updated value
+	 */
+	public long decrementAndGet()
+	{
+		long value = 0;
+
+		// TODO -- you fill in here
+		mRWLock.writeLock().lock();
+		
+		mValue--;
+		value = get();
+		
+		mRWLock.writeLock().unlock();
+		//value = get();
+		
+		return value;
+	}
 
 
-    /**
-     * The ReentrantReadWriteLock used to serialize access to mValue.
-     */
-    // TODO - replace the null with the appropriate initialization:
+	/**
+	 * @brief Atomically increments by one the current value
+	 *
+	 * @returns the previous value
+	 */
+	public long getAndIncrement()
+	{
+		long value = 0;
 
-    /**
-     * Creates a new SimpleAtomicLong with the given initial value.
-     */
-    public SimpleAtomicLong(long initialValue) {
-        // TODO - you fill in here
-    }
+		// TODO -- you fill in here
 
-    /**
-     * @brief Gets the current value
-     * 
-     * @returns The current value
-     */
-    public long get() {
-        // TODO - you fill in here
-    }
+		//value = get();
+		mRWLock.writeLock().lock();
 
-    /**
-     * @brief Atomically decrements by one the current value
-     *
-     * @returns the updated value
-     */
-    public long decrementAndGet() {
-        // TODO - you fill in here
-    }
+		value = get();
+		mValue++;
+		
+		mRWLock.writeLock().unlock();
+		
+		return value;
+	}
 
-    /**
-     * @brief Atomically increments by one the current value
-     *
-     * @returns the previous value
-     */
-    public long getAndIncrement() {
-        // TODO - you fill in here
-    }
+	/**
+	 * @brief Atomically decrements by one the current value
+	 *
+	 * @returns the previous value
+	 */
+	public long getAndDecrement()
+	{
+		long value = 0;
 
-    /**
-     * @brief Atomically decrements by one the current value
-     *
-     * @returns the previous value
-     */
-    public long getAndDecrement() {
-        // TODO - you fill in here
-    }
+		// TODO -- you fill in here
 
-    /**
-     * @brief Atomically increments by one the current value
-     *
-     * @returns the updated value
-     */
-    public long incrementAndGet() {
-        // TODO - you fill in here
-    }
+		//value = get();
+		mRWLock.writeLock().lock();
+
+		value = get();
+		mValue--;
+		
+		mRWLock.writeLock().unlock();
+		
+		return value;
+	}
+
+	/**
+	 * @brief Atomically increments by one the current value
+	 *
+	 * @returns the updated value
+	 */
+	public long incrementAndGet()
+	{
+		long value = 0;
+
+		// TODO -- you fill in here
+
+		mRWLock.writeLock().lock();
+		
+		mValue++;
+		value = get();
+		
+		mRWLock.writeLock().unlock();
+		//value = get();
+		
+		return value;
+	}
 }
 
--- assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,8 +1,9 @@
 package edu.vuum.mocca;
 
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.Lock;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @class SimpleSemaphore
@@ -16,23 +17,32 @@ public class SimpleSemaphore {
      * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
+	Lock mRLock;
 
     /**
      * Define a ConditionObject to wait while the number of
      * permits is 0.
      */
     // TODO - you fill in here
+	Condition noPermits;
 
     /**
      * Define a count of the number of available permits.
      */
     // TODO - you fill in here.  Make sure that this data member will
     // ensure its values aren't cached by multiple Threads..
+	private int mPermits;
+	
+	private Semaphore mSema;
 
-    public SimpleSemaphore(int permits, boolean fair) {
-        // TODO - you fill in here to initialize the SimpleSemaphore,
-        // making sure to allow both fair and non-fair Semaphore
-        // semantics.
+	
+	public SimpleSemaphore(int permits, boolean fair) {
+    	
+    	mSema = new Semaphore(permits, fair);
+    	mPermits = permits;
+    	mRLock = new ReentrantLock(fair);
+    	noPermits = mRLock.newCondition();
+
     }
 
     /**
@@ -41,6 +51,16 @@ public class SimpleSemaphore {
      */
     public void acquire() throws InterruptedException {
         // TODO - you fill in here.
+    	mRLock.lockInterruptibly();
+    	try {
+    		while (availablePermits() == 0) {
+    			noPermits.await();
+    		}
+    		mSema.acquire();
+    		mPermits--;
+    	} finally {
+    		mRLock.unlock();
+    	}
     }
 
     /**
@@ -49,6 +69,16 @@ public class SimpleSemaphore {
      */
     public void acquireUninterruptibly() {
         // TODO - you fill in here.
+    	mRLock.lock();
+    	try {
+    		while (availablePermits() == 0) {
+    			noPermits.awaitUninterruptibly();
+    		}
+    		mSema.acquireUninterruptibly();
+    		mPermits--;
+    	} finally {
+    		mRLock.unlock();
+    	}
     }
 
     /**
@@ -56,6 +86,14 @@ public class SimpleSemaphore {
      */
     void release() {
         // TODO - you fill in here.
+    	try {
+    		mRLock.lock();
+    		mSema.release();
+    		mPermits++;
+    		//noPermits.notify();
+    	} finally {
+    		mRLock.unlock();
+    	}
     }
 
     /**
@@ -64,6 +102,6 @@ public class SimpleSemaphore {
     public int availablePermits() {
         // TODO - you fill in here by changing null to the appropriate
         // return value.
-        return null;
+        return mPermits;
     }
 }
